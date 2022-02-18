8b971ad5f83b9ad1786500d6d47336f706cccc17
Kirill Gromyko <kgromyko@aras.com>
2022-02-18 11:29:01 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
+
+// Styles
+import './App.css'
+
+// Pages and components
+import Dashboard from './pages/dashboard/Dashboard'
+import Login from './pages/login/Login'
+import Project from './pages/project/Project'
+import Signup from './pages/signup/Signup'
+import Create from './pages/create/Create'
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import OnlineUsers from './components/OnlineUsers';
+
+function App() {
+  const { user, authIsReady } = useAuthContext()
+
+  return (
+    <div className="App">
+      {authIsReady && (
+        <BrowserRouter>
+          {user && <Sidebar />}
+          <div className="container">
+            <Navbar />
+            <Routes>
+              <Route
+                path="/"
+                element={user ? <Dashboard /> : <Navigate to="login" />}
+              />
+              <Route
+                path="/create"
+                element={user ? <Create /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/projects/:id"
+                element={user ? <Project /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/login"
+                element={user ? <Navigate to="/" /> : <Login />}
+              />
+              <Route
+                path="/signup"
+                element={user ? <Navigate to="/" /> : <Signup />}
+              />
+            </Routes>
+          </div>
+          {user && <OnlineUsers />}
+        </BrowserRouter>
+      )
+      }
+    </div >
+  );
+}
+
+export default App
